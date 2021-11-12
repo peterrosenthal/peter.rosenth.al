63db7162c5c66a4b56cdc3a93fa6bfbeac7bfd05
Peter Rosenthal <peter@rosenth.al>
2021-11-11 19:37:05 -0700

    new sveltekit project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,8 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	extends: ['eslint:recommended'],
+	plugins: ['svelte3'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2019
@@ -16,5 +11,28 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	rules: {
+		'camelcase': 'error',
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': 'error',
+		'eol-last': ['error', 'always'],
+		'eqeqeq': ['error', 'smart'],
+		'func-style': ['error', 'declaration'],
+		'indent': ['error', 2, { 'ShiftCase': 1 }],
+		'linebreak-style': ['error', 'unix'],
+		'max-len': [
+			'error',
+			{
+				code: 95,
+				ignoreComments: false,
+				ignoreStrings: false,
+				ignoreTemplateLiterals: false,
+				ignoreUrls: true,
+			},
+		],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+		'space-in-parens': ['error', 'never'],
+	},
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  .prelude {
+    font-weight: 400;
+    font-size: 0.55em;
+  }
+</style>
+
+<svelte:head>
+  <title>Peter Rosenthal</title>
+</svelte:head>
+
+<h1><span class='prelude'>Hello, I'm<br></span>Peter Rosenthal</h1>
+<p>a creative everything developer!</p>
