aca5de4cd9c59bfc1965707c9978d5ede551a5d5
Peter Rosenthal <peter@rosenth.al>
2021-11-13 18:38:37 -0700

    static adapter for deployment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,28 @@
 <style>
-  .prelude {
-    font-weight: 400;
-    font-size: 0.55em;
+  h1 {
+    margin: 0 auto;
+    text-align: center;
+    width: 80%;
+    max-width: 28em;
+    font-family: 'Lato', sans-serif;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  #prelude {
+    box-sizing: border-box;
+    padding: 0 .75em;
+    font-weight: 300;
+    text-align: left;
+  }
+
+  #postlude {
+    box-sizing: border-box;
+    padding: 0 0.75em;
+    font-weight: 300;
+    text-align: right;
   }
 </style>
 
@@ -9,5 +30,8 @@
   <title>Peter Rosenthal</title>
 </svelte:head>
 
-<h1><span class='prelude'>Hello, I'm<br></span>Peter Rosenthal</h1>
-<p>a creative everything developer!</p>
+<h1>
+  <div id='prelude'>Hello, I'm</div>
+  <img src='logo.png' alt='Peter Rosenthal'>
+  <div id='postlude'>a creative everything developer!</div>
+</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,24 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		// set base path for deployment
+		paths: {
+			base: dev ? '' : '/dev2',
+		},
+		// static adapter for deployment
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
+	},
 };
 
 export default config;
