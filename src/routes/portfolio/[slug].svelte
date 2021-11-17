c35172861f5076fc414c489a095c34c7c9f03c58
Peter Rosenthal <peter@rosenth.al>
2021-11-16 19:18:35 -0700

    google docs dynamic content

--- src/routes/portfolio/[slug].svelte
+++ src/routes/portfolio/[slug].svelte
@@ -1,27 +1,68 @@
-<script context="module">
+<script context='module'>
   import * as archieml from 'archieml';
 
   export async function load({ page, fetch, session, stuff }) {
     const slug = page.params.slug;
-    const url = 'https://docs.google.com/document/d/1ClXruC-rmLh2iCEpRHa1kG_6X4FSh935Zo6nCDPuWCI/export?format=txt';
-    const response = await fetch(url);
+    const entriesUrl = 'https://docs.google.com/document/d/1ClXruC-rmLh2iCEpRHa1kG_6X4FSh935Zo6nCDPuWCI/export?format=txt';
+    const entriesResponse = await fetch(entriesUrl);
 
-    if (response.ok) {
-      const text = await response.text();
-      const aml = archieml.load(text);
-      if (aml[slug] !== undefined) {
-        return { props: { text: aml.slug } };
+    if (entriesResponse.ok) {
+      const entriesResponseText = await entriesResponse.text();
+      const entries = archieml.load(entriesResponseText);
+      if (entries[slug] !== undefined) {
+        const entryUrl = `https://docs.google.com/document/d/${entries[slug]}/export?format=txt`;
+        const entryResponse = await fetch(entryUrl);
+        if (entryResponse.ok) {
+          const entryResponseText = await entryResponse.text();
+          const entry = archieml.load(entryResponseText);
+          return { props: { contents: entry } };
+        }
+        return {
+          status: entryResponse.status,
+          error: new Error(`could not load url: ${entryUrl}`),
+        };
+      } else {
+        return {
+          status: 404,
+          error: new Error(`Project not found: ${slug}`),
+        };
       }
     }
     return { 
-      status: response.status,
-      error: new Error(`Could not load ${url}`),
+      status: entriesResponse.status,
+      error: new Error(`Could not load url: ${entryiesUrl}`),
     };
   }
 </script>
 
 <script>
-  export let text = 'default';
+  export let contents = {
+    title: 'Default Entry Title',
+    contents: [
+      {
+        type: 'paragraph',
+        text: 'Default paragraph text.'
+      },
+      {
+        type: 'subheading',
+        text: 'Default subheading text.'
+      },
+      {
+        type: 'image',
+        url: 'http://example.com/',
+        alt: 'Default image alt text.'
+      },
+    ]
+  };
 </script>
 
-<p>{text}</p>
+<h1>{contents.title}</h1>
+{#each contents.contents as entry}
+  {#if entry.type == 'image'}
+    <img src={entry.url} alt={entry.alt} />
+  {:else if entry.type == 'subheading'}
+    <h2>{entry.text}</h2>
+  {:else if entry.type == 'paragraph'}
+    <p>{entry.text}</p>
+  {/if}
+{/each}
--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -1,1 +1,47 @@
-Portfolio!
+<script context='module'>
+  import * as archieml from 'archieml';
+
+  export async function load({ page, fetch, session, stuff }) {
+    const entriesUrl = 'https://docs.google.com/document/d/1ClXruC-rmLh2iCEpRHa1kG_6X4FSh935Zo6nCDPuWCI/export?format=txt';
+    const entriesResponse = await fetch(entriesUrl);
+
+    const projects = [];
+
+    if (entriesResponse.ok) {
+      const entriesResponseText = await entriesResponse.text();
+      const entries = archieml.load(entriesResponseText);
+      for (const entrySlug in entries) {
+        const entryUrl = `https://docs.google.com/document/d/${entries[entrySlug]}/export?format=txt`;
+        const entryResponse = await fetch(entryUrl);
+        if (entryResponse.ok) {
+          const entryResponseText = await entryResponse.text();
+          const entry = archieml.load(entryResponseText);
+          projects.push({
+            title: entry.title,
+            thumbnail: entry.thumbnail,
+          });
+        } else {
+          return {
+            status: entryResponse.status,
+            error: new Error(`could not load url: ${entryUrl}`),
+          };
+        }
+      }
+      return { props: { projects } };
+    }
+    return { 
+      status: entriesResponse.status,
+      error: new Error(`Could not load url: ${entriesUrl}`),
+    };
+  }
+</script>
+
+<script>
+  export let projects = [];
+</script>
+
+<ul>
+  {#each projects as project}
+    <li>{project.title}<img src={project.thumbnail.url} alt={project.thumbnail.alt} /></li>
+  {/each}
+</ul>
