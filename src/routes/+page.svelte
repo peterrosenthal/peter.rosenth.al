981b2fdf8f193ffd011d625c54f96f47e2b55069
Peter Rosenthal <peter@rosenth.al>
2024-05-17 19:42:51 -0600

    new personal logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,14 @@
   console.log(aboutme);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  img {
+    width: 1200px;
+  }
+</style>
+
+<h1>
+  <div>Hello, I'm</div>
+  <img src='/logo.svg' alt='Peter Rosenthal' />
+  <div>a creative software developer</div>
+</h1>
