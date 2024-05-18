c203c63af518009ef6bb439f386bd90545afd4ac
Peter Rosenthal <peter@rosenth.al>
2024-05-18 16:50:29 -0600

    add color system to root style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,77 @@
+<style>
+  :root {
+    --base-0: #FBFAF9;
+    --base-10: #F2F0ED;
+    --base-20: #E2DFD9;
+    --base-30: #D1CCC7;
+    --base-40: #BDB7B2;
+    --base-50: #A19897;
+    --base-60: #857A7B;
+    --base-70: #6A585D;
+    --base-80: #4E363E;
+    --base-90: #39222C;
+    --base-100: #301822;
+
+    --red-0: #F5EDEA;
+    --red-10: #EEDDD8;
+    --red-20: #E7CCC5;
+    --red-30: #E4B5AA;
+    --red-40: #DD9383;
+    --red-50: #D87D69;
+    --red-60: #D56A53;
+    --red-70: #D5593F;
+    --red-80: #D14629;
+    --red-90: #BF3D22;
+    --red-100: #B1351B;
+
+    --orange-0: #F7E3D4;
+    --orange-10: #F5DBC6;
+    --orange-20: #F2D0B5;
+    --orange-30: #F1C5A2;
+    --orange-40: #EEB181;
+    --orange-50: #EA9E61;
+    --orange-60: #E78A40;
+    --orange-70: #E67C28;
+    --orange-80: #E27118;
+    --orange-90: #DE6C12;
+    --orange-100: #D7670F;
+
+    --yellow-0: #F0E9C7;
+    --yellow-10: #ECE3B6;
+    --yellow-20: #E9DEA5;
+    --yellow-30: #E8DC9B;
+    --yellow-40: #E6D789;
+    --yellow-50: #E2CF6E;
+    --yellow-60: #E1CB60;
+    --yellow-70: #E0C74D;
+    --yellow-80: #E0C438;
+    --yellow-90: #E4C321;
+    --yellow-100: #E0BE15;
+
+    --green-0: #D7E0D7;
+    --green-10: #CAD8CA;
+    --green-20: #BCD2BC;
+    --green-30: #AECDAD;
+    --green-40: #9FC79E;
+    --green-50: #90C18F;
+    --green-60: #82BC80;
+    --green-70: #73B672;
+    --green-80: #62AF60;
+    --green-90: #55A752;
+    --green-100: #4E9A4C;
+
+    --blue-0: #CCD5DB;
+    --blue-10: #BECCD5;
+    --blue-20: #ACC0CD;
+    --blue-30: #99B4C7;
+    --blue-40: #8AACC3;
+    --blue-50: #7AA2BD;
+    --blue-60: #6B99B8;
+    --blue-70: #598DB1;
+    --blue-80: #4D80A3;
+    --blue-90: #457392;
+    --blue-100: #3C6580;
+  }
+</style>
+
+<slot></slot>
