39113cbf45bd6defaae0bd459d5c31883148bd71
Peter Rosenthal <peter@rosenth.al>
2024-05-21 19:27:05 -0600

    add aboutme page

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,7 +1,42 @@
 <script>
   import Nav from '$lib/Nav.svelte';
+  import aboutme from '$data/aboutme.json';
 </script>
 
-<Nav page="me" />
+<style>
+  .container {
+    width: 90%;
+    max-width: 65rem;
+    min-height: calc(100vh - 8rem);
+    box-sizing: border-box;
+    margin: 8rem auto 0;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
 
-<h1>About me</h1>
+  h1 {
+    font-size: 3rem;
+    margin: 0;
+    padding: 0;
+    color: var(--baseline-80);
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+    color: var(--baseline-100);
+  }
+</style>
+
+<Nav page='me' />
+
+<div class='container'>
+  <h1>{aboutme.title}</h1>
+
+  {#each aboutme.paragraphs as paragraph}
+    <p>{paragraph}</p>
+  {/each}
+</div>
